194126d74049655bc0d0499c7207fdd63fa22a3b
Liliana <liliana@lili.(none)>
2011-01-30 10:41:55 -0430

    Inicio proyecto

---
+++ accions/MostrarEntidades.java
@@ -0,0 +1,30 @@
+package accions;
+import java.util.Vector;
+
+import beans.Atributo;
+import beans.Entidad;
+
+public class MostrarEntidades {
+	public void imprimirEntidades(Vector<Entidad> entidades){
+		int i =entidades.size()-1;
+		int j = 0;
+		while(i>=0){
+			
+			System.out.println(i);
+			System.out.println("-- "+entidades.get(i).getNombre_entidad()+" --");
+			j = entidades.get(i).getAtributos().size()-1;
+			Vector<Atributo> atributos = entidades.get(i).getAtributos();
+			while(j>=0){
+			System.out.println("	Nombre : "+ atributos.get(j).nombre);
+			System.out.println("		Tipo : "+ atributos.get(j).tipo);
+			System.out.println("		Valor : "+ atributos.get(j).valor);
+			System.out.println("		Nulo : "+ atributos.get(j).nulo);
+			
+			j--;
+			}i--;
+			
+		}
+		
+	}
+
+}
---
+++ beans/Atributo.java
@@ -0,0 +1,50 @@
+package beans;
+
+import java.util.Vector;
+
+public class Atributo {
+	public String nombre = null;
+	public String tipo = null;
+	public String valor = null;
+	public int longitud = 0;
+	public boolean nulo =  true;
+	public Vector<String> dominio;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+	public int getLongitud() {
+		return longitud;
+	}
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+	public boolean isNulo() {
+		return nulo;
+	}
+	public void setNulo(boolean nulo) {
+		this.nulo = nulo;
+	}
+	public Vector<String> getDominio() {
+		return dominio;
+	}
+	public void setDominio(Vector<String> dominio) {
+		this.dominio = dominio;
+	}
+}
+
