5b540af7468f519787624ed510c4e23bae24e1dd
Chelo <unoryche@gmail.com>
2011-01-30 14:02:28 -0430

    cambios de chelo y karina 30/01/11

--- beans/Entidad.java
+++ beans/Entidad.java
@@ -1,11 +1,16 @@
 package beans;
 
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class Entidad {
 	
 	public String nombre_entidad;
 	public Vector<Atributo> atributos;
+	public Vector<Atributo> referencias;
+	public String clave;
+	public Vector<ArrayList<String>> foraneo; 
+	public String tipo;
 	
 	public String getNombre_entidad() {
 		return nombre_entidad;
@@ -14,7 +19,39 @@ public class Entidad {
 	public void setNombre_entidad(String nombre_entidad) {
 		this.nombre_entidad = nombre_entidad;
 	}
+	
+	public Vector<Atributo> getReferencias() {
+		return referencias;
+	}
+	
+	public void setReferencias(Vector<Atributo> newreferencias) {
+		this.referencias = newreferencias;
+	}
 
+	public String getClave() {
+		return clave;
+	}
+	
+	public Vector<ArrayList<String>> getForaneo() {
+		return foraneo;
+	}
+
+	public void setForaneo(Vector<ArrayList<String>> foraneo) {
+		this.foraneo = foraneo;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
 	public Vector<Atributo> getAtributos() {
 		return atributos;
 	}
