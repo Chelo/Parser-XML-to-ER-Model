3e7601567fd78ad3ffc39dc9d83b79f18cc49c21
daniepusb <danie.p.usb@gmail.com>
2011-02-15 17:12:41 -0430

    ParserXML.java -- daniel --arreglos generales

--- accions/ParserXML.java
+++ accions/ParserXML.java
@@ -1,10 +1,11 @@
 package accions;
 
+//import beans.Atributo;
+//import beans.OrigenXML;
+
 import java.io.FileReader;
 import java.io.IOException;
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.xml.sax.*;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
@@ -20,38 +21,71 @@ import org.xml.sax.helpers.XMLReaderFactory;
 public class ParserXML extends DefaultHandler {
 
 	private final static String xmlFileName    = "libro.xml";
-	private final static String targetFileName = "insert.sql";
-    private final static int identificador = 0;
-	
+	//private final static String targetFileName = "insert.sql";
+	private static String contenido      = "";
+	private static int    indent         = 0;
+    
+	private final XMLReader xr;
     
     
-    /** Crear una instancia de ParserXML 
-     *
-    */
-    public ParserXML() {
-    	
-    	
-    	
-    	
-    	
-        //Parsear archivo xml
-    	//devuelve un tipo Document (usa libreria Dom)
-        //Document doc = parseFile(xmlFileName);
-        
-        //Obtener la raiz del arbol xml
-        //Node raiz = doc.getDocumentElement();
-        
-        //Imprimir contenido XML
-        System.out.println("______________________________\n");
-        //writeDocumentToOutput(raiz,0,identificador);
-        //insertScript();
-        System.out.println("______________________________\n");
+    //En el main la llamada sera algo del tipo: 
+    //new ParserXML();
+    //ParserXML Pxml = new ParserXML(); 
 
-        //Crear el archivo sql insert
-        //saveXMLDocument(targetFileName, doc);
-       
+    /** Crear una instancia de ParserXML */
+    public ParserXML() throws SAXException {
+    		xr = XMLReaderFactory.createXMLReader();
+	        xr.setContentHandler(this);
+	        xr.setErrorHandler(this);
+	        System.out.println("ParserXML--");
     }
-	
-	
-	
+    public void leer(final String archivoXML) throws IOException, SAXException {
+        FileReader fr = new FileReader(archivoXML);
+        System.out.println("Leer--");
+        xr.parse(new InputSource(fr));
+    }
+    
+    private String getIndentSpaces(int indent) {
+        StringBuffer buffer = new StringBuffer();
+        for (int i = 0; i < indent; i++) {
+            buffer.append(" ");
+        }
+        return buffer.toString();
+    }
+    @Override
+    public void startDocument() {System.out.println("<XML>\n");}
+    @Override
+    public void endDocument() {System.out.println("</XML>");}
+    
+    @Override
+    public void startElement(String uri, String name, String qName, Attributes atts) {
+    	//hallar indentacion ,calcular e imprimir 
+    	indent++;
+    	//imprimir indentacion
+    	System.out.print(getIndentSpaces(indent) +"<" + name + ">");
+        //crear new
+        //OrigenXML temp = new OrigenXML();
+    }
+    @Override
+    public void endElement(String uri, String name, String qName) {
+    	System.out.print(getIndentSpaces(indent) + "</" + name + ">");
+    	indent--;
+    }
+    public void characters(char buf[], int offset, int len) throws SAXException {
+    	indent=indent+2;
+    	//ojo revisar cuando es fecha 
+    	//guardar en un String y luego almacenar en OrigenXML
+    	contenido = new String(buf, offset, len);
+    	System.out.print(getIndentSpaces(indent) + contenido);
+    	indent=indent-2;
+    }
+    
+    /** Starts XML parsing example
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SAXException, IOException {
+    	ParserXML pxml = new ParserXML();
+    	pxml.leer(xmlFileName);
+    }
+    
 }//fin clase ParserXML
