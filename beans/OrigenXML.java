46729e9eca36ab095c41d4de93e65486b7e72d2d
daniepusb <danie.p.usb@gmail.com>
2011-02-15 16:08:06 -0430

    OrigenXMl.java, -- daniel -- estructura para almacenar los datos por cada tag --Ojo la estructura es temporal

---
+++ beans/OrigenXML.java
@@ -0,0 +1,16 @@
+package beans;
+
+public class OrigenXML {
+
+	private String nombreTag;
+
+		public void setNombreTag(String titulo) {
+			this.nombreTag = titulo;
+		}
+		public String getnombreTag() {
+			return nombreTag;
+		}
+		
+		
+
+	}
