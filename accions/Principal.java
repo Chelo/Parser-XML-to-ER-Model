5a3ecfc86b9523fda6a44b70d4ba6a93ff9ae29c
Chelo <unoryche@gmail.com>
2011-02-18 10:24:35 -0430

    acomode unas cosas que cambie del principal, ya todo esta normal!

--- accions/Principal.java
+++ accions/Principal.java
@@ -8,13 +8,13 @@ public class Principal {
 	
 	public static void main(String[] args) throws SAXException, IOException {
 				
-		Parser.ParsearXMLSchema("ejemplo2.xml");
+		Parser.ParsearXMLSchema("ejemplo.xml");
 		//Estas tres líneas son solo para probar que dentro del main se puede 
 		//utilizar el hash de entidades (las pueden borrar)
 		//Para hacer uso del hash simplemente hacen Parser.entidades
-		//System.out.print("Tamano del Hash de entidades/ Numero de entidades "+Parser.entidades.size()+ "\n");
-		//System.out.print("Nombre de la primera entidad "+Parser.entidades.get("User").getNombre_entidad()+"\n");
-		//System.out.print("Nombre de su primer atributo "+Parser.entidades.get("User").getAtributos().get(0).getNombre()+"\n");
+		System.out.print("Tamano del Hash de entidades/ Numero de entidades "+Parser.entidades.size()+ "\n");
+		System.out.print("Nombre de la primera entidad "+Parser.entidades.get("User").getNombre_entidad()+"\n");
+		System.out.print("Nombre de su primer atributo "+Parser.entidades.get("User").getAtributos().get(0).getNombre()+"\n");
 
 		ParserXML.ParsearXML("libro.xml");
 	}
