97763583b519cd838b12d86d16e5e24fbd66b249
Chelo <unoryche@gmail.com>
2011-02-24 13:29:50 -0430

    X

--- accions/Principal.java
+++ accions/Principal.java
@@ -21,14 +21,14 @@ public class Principal {
 		//Estas líneas son solo para probar el nuevo hash que necesita Daniel,(nombreEntidad, tipoEntidad) 
 		//(Luego se pueden borrar estas líneas)
 		//Para hacer uso del hash simplemente hacen Parser.nombreEntidades
-		Iterator<String> nombres = Parser.nombreEntidades.keySet().iterator();
-		Iterator<String> tipos = Parser.nombreEntidades.values().iterator();
+		//Iterator<String> nombres = Parser.nombreEntidades.keySet().iterator();
+		//Iterator<String> tipos = Parser.nombreEntidades.values().iterator();
 		
-		while(nombres.hasNext() & tipos.hasNext())
-		{	
-			System.out.print("Nombre "+nombres.next()+"\n");
-			System.out.print("Tipo "+tipos.next() +"\n");
-		}
+		//while(nombres.hasNext() & tipos.hasNext())
+		//{	
+			//System.out.print("Nombre "+nombres.next()+"\n");
+			//System.out.print("Tipo "+tipos.next() +"\n");
+		//}
 
 		//ParserXML.ParsearXML("libro.xml");
 	}
